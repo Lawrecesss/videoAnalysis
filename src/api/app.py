a42bbc124491883458fa2e3d75129b00a00eaabe
Lawrence <lhshein14@gmail.com>
2026-02-05 19:30:07 +0800

    added middleware for CORS (cross-origin resource sharing)

--- src/api/app.py
+++ src/api/app.py
@@ -3,13 +3,21 @@ from fastapi import FastAPI, UploadFile, File, HTTPException, WebSocket
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 from fastapi.requests import Request
-
+from fastapi.middleware.cors import CORSMiddleware
 from src.services.websocket import websocket_result_handler
 from src.core.client import upload_video
 
 BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), "../.."))
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # or ["http://localhost:3000"] for dev
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 templates = Jinja2Templates(
     directory=os.path.join(BASE_DIR, "templates")
