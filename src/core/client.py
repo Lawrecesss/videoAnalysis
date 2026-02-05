7ae7b7e887466eb40276b17db77c346b2a1b299c
Lawrence <lhshein14@gmail.com>
2026-02-05 15:40:37 +0800

    trying to fix internal error

--- src/core/client.py
+++ src/core/client.py
@@ -1,16 +1,20 @@
 import os
 import uuid
+from fastapi import UploadFile
 from src.core.utils import enqueue_video_for_processing
 
 TMP_ROOT = "./tmp"
 os.makedirs(TMP_ROOT, exist_ok=True)
 
-async def upload_video(file: str) -> str:
+async def upload_video(file: UploadFile) -> str:
     job_id = str(uuid.uuid4())
+
     job_dir = os.path.join(TMP_ROOT, job_id)
     os.makedirs(job_dir, exist_ok=True)
 
     video_path = os.path.join(job_dir, "input.mp4")
+
+    await file.seek(0)
 
     with open(video_path, "wb") as f:
         while True:
@@ -20,4 +24,5 @@ async def upload_video(file: str) -> str:
             f.write(chunk)
 
     enqueue_video_for_processing(job_id)
+
     return job_id
